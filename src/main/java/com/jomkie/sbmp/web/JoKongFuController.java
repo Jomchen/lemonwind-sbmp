d4e8dda6ad586b90446f1396f6fc309796d97105
Jomkie <jomkiemail@gmail.com>
2023-09-14 14:55:59 +0800

    修改类名

--- src/main/java/com/jomkie/sbmp/web/JoKongFuController.java
+++ src/main/java/com/jomkie/sbmp/web/JoKongFuController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-public class KongFuController {
+public class JoKongFuController {
 
     @Autowired
     private KongFuService kongFuService;
